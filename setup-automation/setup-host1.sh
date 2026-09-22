dfefacd414819e6e2e15b9cd0917a40690a20792
Molly Rudisill <mrudisil@redhat.com>
2026-09-22 16:34:54 -0400

    fixed echo

--- setup-automation/setup-host1.sh
+++ setup-automation/setup-host1.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 USER=rhel
 
-echo "Adding wheel" > /root/post-run.log
-usermod -aG wheel rhel
-
-echo "Setup vm control01" > /tmp/progress.log
-
-chmod 666 /tmp/progress.log 
-
-#dnf install -y nc
-
+echo "hi"
