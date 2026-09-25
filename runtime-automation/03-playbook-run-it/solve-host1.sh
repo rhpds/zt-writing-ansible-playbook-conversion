e37ac04786320e25427b82289c94495523b0ae93
Molly Rudisill <mrudisil@redhat.com>
2026-09-25 11:13:25 -0400

    updating solve

--- runtime-automation/03-playbook-run-it/solve-host1.sh
+++ runtime-automation/03-playbook-run-it/solve-host1.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 WORKSPACE=/home/rhel/ansible-files
 
 runuser -u rhel -- env HOME=/home/rhel XDG_RUNTIME_DIR="/run/user/$(id -u rhel)" \
-  bash -c "cd \"$WORKSPACE\" && ansible-navigator run system_setup.yml --mode stdout"
+  bash -c "cd \"$WORKSPACE\" && ansible-navigator run system_setup.yml"
 
 echo "Ran system_setup.yml."
